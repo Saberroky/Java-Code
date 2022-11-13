a1a8b36dc90338bc713896644bde5b5f66f6abbf
Saberroky <94374083+Saberroky@users.noreply.github.com>
2022-11-13 13:41:08 +0600

    data type of java

---
+++ DataTypes.java
@@ -0,0 +1,33 @@
+import java.sql.SQLOutput;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Boolean data type
+        boolean flag = true;
+        System.out.println("This is boolean type data: "+flag);
+        //byte data type
+        byte range;
+        range = 123;
+        System.out.println("this is byte data type: " + range);
+        //short type
+        short temperature;
+        temperature = -100;
+        System.out.println("the tempreture is : "+temperature);
+        //int type
+        int ranges = -73254;
+        System.out.println("The int is: " +ranges);
+        //double type
+        double number = 763.766;
+        System.out.println("the double numbner is :"+number);
+        //float type
+        float number2 = 67.9876f;
+        System.out.println("the float number is: "+ number2);
+        //char type
+        char latter = '\u0052';
+        System.out.println("this is char: "+latter);
+        //string type
+        String hd = "plz help me";
+        System.out.println("this is string: "+hd);
+    }
+
+}
