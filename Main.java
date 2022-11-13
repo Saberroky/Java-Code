378309e2980e06aeefce27e6b95565213198acb5
Saberroky <94374083+Saberroky@users.noreply.github.com>
2022-11-13 13:09:11 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//package com.hw;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        String  name = "Saber Hossain Rocky";
+        int age = 23;
+        System.out.println("Name: "+name
+        +" \nAge: "+age);
+
+    }
+}
